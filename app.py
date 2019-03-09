6f94cbdb9549b4730aa6d6495ca931e9d8422918
deterministic-forest <jwilli18@go.olemiss.edu>
2019-03-09 15:23:42 -0600

    Changed posting system to HTML form, added edit function

--- app.py
+++ app.py
@@ -1,42 +1,34 @@
 from flask import Flask, render_template, request
 import sqlite3
 
+
 app = Flask(__name__)
-
-
-def content():
-    conn = sqlite3.connect('posts.sqlite')
-    c = conn.cursor()
-    c.execute('SELECT * FROM Posts')
-    return c
 
 
 @app.route('/')
 def main_page():
-    pages = [article for article in content()]
+    from .content_management import get_all_posts
+    pages = get_all_posts()
     rows = [pages[i:i + 3] for i in range(0, len(pages), 3)]
     return render_template('index.html', data=rows)
 
 
-@app.route('/new-post', methods=['POST'])
+@app.route('/insert', methods=['POST', 'GET'])
 def new_post():
-    conn = sqlite3.connect('posts.sqlite')
-    c = conn.cursor()
-    data = request.get_json()
-    title = data['title']
-    description = data['description']
-    symlink = data['symlink']
-    thumb = data['thumbnail']
-    tags = data['tags']
-    content = data['content']
-    try:
-        c.execute('INSERT INTO Posts VALUES(?, ?, ?, ?, ?, ?)',
-                  (title, description, symlink, thumb, tags, content))
-    except Exception as e:
-        return e
-    conn.commit()
-    conn.close()
-    return 'Success!'
+    from .content_management import get_all_posts, new_post
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        symlink = request.form['symlink']
+        thumb = request.form['thumbnail']
+        tags = request.form['tags']
+        contents = request.form['contents']
+        new_post('INSERT INTO Posts VALUES(?, ?, ?, ?, ?, ?)',
+                  (title, description, symlink, thumb, tags, contents))
+    results = get_all_posts()
+    for result in results:
+        print(result['title'])
+    return render_template('crud.html', results=results)
 
 
 @app.route('/blog/<post>')
@@ -46,6 +38,46 @@ def render_post(post):
     url = f'/blog/{post}'
     c.execute('SELECT * FROM Posts WHERE symlink IS ?', (url,))
     return render_template('post.html', data=c.fetchone())
+
+
+@app.route('/edit_entry', methods=['POST', 'GET'])
+def edit_entry():
+    from .content_management import get_all_posts, edit_post
+    if request.method == 'GET':
+        query_title = request.args.get('edit_title')
+        editables = edit_post('SELECT * FROM Posts WHERE title IS ?', (query_title,))
+    results = get_all_posts()
+    return render_template('crud.html', results=results, editables=editables)
+
+
+@app.route('/edit', methods=['POST', 'GET'])
+def submit_edit():
+    '''TODO: Add completion message to this and delete'''
+    from .content_management import get_all_posts, new_post
+    if request.method == 'POST':
+        old_entry = request.form['old_entry']
+        title = request.form['title']
+        description = request.form['description']
+        symlink = request.form['symlink']
+        thumb = request.form['thumbnail']
+        tags = request.form['tags']
+        contents = request.form['contents']
+        new_post('''UPDATE Posts SET title=?, description=?, symlink=?,
+                  thumbnail=?, tags=?, content=? WHERE title IS ?''',
+                  (title, description, symlink, thumb, tags, contents, old_entry))
+        return 'Good job bro'
+    results = get_all_posts()
+    render_template('crud.html', results=results)
+
+
+@app.route('/delete_entry', methods=['POST', 'GET'])
+def delete_entry():
+    from .content_management import get_all_posts, delete_post
+    if request.method == 'GET':
+        victim = request.args.get('del_title')
+        delete_post('DELETE FROM Posts WHERE title IS ?', (victim,))
+    results = get_all_posts()
+    return render_template('crud.html', results=results)
 
 
 if __name__ == '__main__':
--- content_management.py
+++ content_management.py
@@ -16,29 +16,57 @@ def create_db():
 
 
 @command()
-@option('--title', prompt='Post title:')
-@option('--description', prompt='Description:')
-@option('--thumb', prompt='Thumbnail image address:')
-@option('--tags', prompt='Post tags:')
-@option('--path', prompt='Path to HTML doc:')
+@option('--title', prompt='Post title')
+@option('--description', prompt='Description')
+@option('--thumb', prompt='Thumbnail image address')
+@option('--tags', prompt='Post tags')
+@option('--path', prompt='Path to HTML doc')
 def add_entry(title, description, thumb, tags, path):
     entry = dict()
+    spaceless_title = title.lower().replace(" ", "_")
     entry['title'] = title
     entry['description'] = description
-    entry['symlink'] = '/blog/{}'.format(title.lower().replace(" ", "_"))
+    entry['symlink'] = '/blog/{}'.format(spaceless_title)
     entry['thumbnail'] = thumb
     entry['tags'] = tags
     with open(path, 'r') as rf:
         entry['content'] = rf.read()
     with open(f'./static/pages/{title}.json', 'w') as wf:
-        json.dump(entry, wf)
+        json.dump(spaceless_title, wf)
 
 
-def content():
-    conn = sqlite3.connect('posts.sqlite')
-    c = conn.cursor()
-    c.execute('SELECT * FROM Posts')
-    return c
+def get_all_posts():
+    with sqlite3.connect('posts.sqlite') as conn:
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
+        c.execute('SELECT * FROM Posts')
+        rows = c.fetchall()
+        return rows
+
+
+def new_post(query, var):
+    with sqlite3.connect('posts.sqlite') as conn:
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
+        c.execute(query, var)
+        conn.commit()
+
+
+def edit_post(query, var):
+    with sqlite3.connect('posts.sqlite') as conn:
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
+        c.execute(query, var)
+        rows = c.fetchall()
+        return rows
+
+
+def delete_post(query, var):
+    with sqlite3.connect('posts.sqlite') as conn:
+        conn.row_factory = sqlite3.Row
+        c = conn.cursor()
+        c.execute(query, var)
+
 
 
 if __name__ == '__main__':
